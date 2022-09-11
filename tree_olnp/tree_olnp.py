726de71fc2dc80841ecfe3b52e4af2f6fc1f6e66
basarbatucan <basarbatucan@gmail.com>
2022-09-12 01:44:12 +0300

    migrated to python

--- tree_olnp/tree_olnp.py
+++ tree_olnp/tree_olnp.py
@@ -1,3 +1,4 @@
+from os import P_ALL
 from platform import node
 import numpy as np
 from sklearn.decomposition import PCA
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 
 class tree_olnp:
 
-    def __init__(self, tfpr_=0.1, eta_init_=0.01, beta_init_=100, sigmoid_h_=-1, Lambda_=0, tree_depth_=2, split_prob_=0.5, node_loss_constant_=-1, projection_type_='PCA', max_x_= None, max_y_ = None) -> None:
+    def __init__(self, tfpr_=0.1, eta_init_=0.01, beta_init_=100, sigmoid_h_=-1, Lambda_=0, tree_depth_=2, split_prob_=0.5, node_loss_constant_=1, projection_type_='PCA', ensemble_type_='probabilistic', max_x_= None, max_y_ = None) -> None:
         
         # hyperparameters
         self.tfpr = tfpr_
@@ -20,6 +21,7 @@ class tree_olnp:
         self.split_prob_ = split_prob_
         self.node_loss_constant_ = node_loss_constant_
         self.projection_type_ = projection_type_
+        self.ensemble_type_ = ensemble_type_
 
         # parameters
         self.w_ = None # perceptron weight
@@ -55,11 +57,26 @@ class tree_olnp:
         tree_depth = self.tree_depth_
         split_prob = self.split_prob_
         node_loss_constant = self.node_loss_constant_
+        projection_type = self.projection_type_
+
+        # data related parameters
+        n_samples, n_features = X.shape
+
+        # context tree, structural parameters
+        number_of_nodes = 2**(tree_depth+1)-1
+
+        # initiate perceptron parameters
+        if projection_type == 'manual':
+            # in manual projection, first 2D of the data is used as coords
+            w = np.random.randn(n_features-2, number_of_nodes)*1e-4
+        else:
+            w = np.random.randn(n_features, number_of_nodes)*1e-4
+        b = np.random.randn(number_of_nodes)*1e-4
 
         # prepare the X
         # note that since this is an online algorithm, to have better conversion, we augment initial data
         # initiate the Fourier features
-        n_samples, n_features = X.shape
+        
         if n_samples<n_samples_augmented_min:
             # augmentation is necessary
             X_, y_ = self.__augment_data(X=X, y=y, n_samples=n_samples, n_features=n_features, n_samples_augmented_min=n_samples_augmented_min)
@@ -72,8 +89,20 @@ class tree_olnp:
         # get number of negative samples after augmentation (if necessary)
         number_of_negative_samples = np.sum(y_==-1)
 
+        # initiate learning rates
+        eta = eta_init
+        beta_init = beta_init/number_of_negative_samples # scaling beta init with the number of total negative samples imrpoves convergence
+        beta = beta_init
+        
+        # create negative sample buffer
+        # this buffer is used to estimate FPR in an online manner during training, 200 is a constant and can be changed
+        negative_sample_buffer_size = max(int(np.round(2/tfpr)), 200)
+        negative_sample_buffer = np.zeros((negative_sample_buffer_size))
+        negative_sample_buffer_index = 0
+
         # generate connectivity matrix which contains the connectivity information of the nodes within the context tree
         self.__generate_connectivity_matrix()
+        connectivity = self.connectivity_
 
         # calculate node centers
         # this information is used to determine how a sample propagates from top to root
@@ -81,7 +110,249 @@ class tree_olnp:
         # User can provide seperate feature set for context tree initialization
         self.__init_partitioner(X)
 
+        P = np.ones((number_of_nodes,), dtype=np.float128)                                # node probability
+        E = np.ones((number_of_nodes,), dtype=np.float128)                                # prediction performance of node n
+        y_discriminant = np.zeros((number_of_nodes,))                  # discriminant of nodes
+        C = np.ones((number_of_nodes,))                                # prediction of nodes
+        node_loss_constant = node_loss_constant*(-320)/n_samples;      # adjustment to the learning rate to prevent numerical error
+        sample_mu = np.zeros((n_samples, tree_depth+1))                # node weight used in ensemble classifier
+        sigma_tree = np.zeros((tree_depth+1,))                            
+        mu_tree = np.zeros((tree_depth+1,))
+
+        # init accumulators
+        tp = 0
+        fp = 0
+        tpr_train_array = np.zeros((n_samples))
+        fpr_train_array = np.zeros((n_samples))
+        neg_class_weight_train_array = np.zeros((n_samples))
+        pos_class_weight_train_array = np.zeros((n_samples))
+        gamma_array = np.zeros((n_samples))
+        transient_number_of_positive_samples = 1
+        transient_number_of_negative_samples = 1
+
+        # initiate weights
+        neg_class_weight_train_array[0] = 2*gamma
+        pos_class_weight_train_array[0] = 2*gamma
+        
+        for i in range(0, n_samples):
+
+            # take the input data
+            xt = X_[i,:]
+            yt = y_[i]
+
+            # calculate sample propogation path through tree
+            dark_node_indices = self.__find_dark_nodes(xt)
+
+            # if manual projection is used, first 2D are coordinates
+            if projection_type == 'manual':
+                xt = xt[2:]
+
+            # make prediction
+            for k in range(0, len(dark_node_indices)):
+                dark_node_index = dark_node_indices[k]
+                # calculate sigma
+                if k == 0:
+                    sigma_tree[k] = 1-split_prob
+                else:
+                    sigma_tree[k] = (1-split_prob)*P[connectivity[dark_node_index, 1]]*sigma_tree[k-1]
+                    if k == len(dark_node_indices)-1:
+                        sigma_tree[k] = sigma_tree[k]/(1-split_prob)
+                # calculate weights
+                mu_tree[k] = sigma_tree[k]*E[dark_node_index]/P[0]
+                if k == len(dark_node_indices)-1:
+                    mu_tree[k] = 1-np.sum(mu_tree[:k])
+
+                # calculate discriminant in each node
+                y_discriminant_ = np.matmul(xt, w[:, dark_node_index])+b[dark_node_index]
+                y_discriminant[dark_node_index] = y_discriminant_
+                C[dark_node_index] = np.sign(y_discriminant_)
+            
+            # calculate prediction of the ensemble
+            yt_predict = self.__expert_ensemble(C[dark_node_indices], y_discriminant[dark_node_indices], mu_tree)
+
+            # save sample mu
+            sample_mu[i, :] = mu_tree
+
+            # save tp and fp
+            if yt == 1:
+                if yt_predict == 1:
+                    tp = tp+1
+            else:
+                if yt_predict == 1:
+                    fp = fp+1
+
+            tpr_train_array[i] = tp/transient_number_of_positive_samples
+            fpr_train_array[i] = fp/transient_number_of_negative_samples
+
+            # save gamma
+            gamma_array[i] = gamma
+
+            # update the buffer with the current prediction
+            if yt == -1:
+
+                # modify the size of the FPR estimation buffer
+                if negative_sample_buffer_index == negative_sample_buffer_size-1:
+                    negative_sample_buffer[:-1] = negative_sample_buffer[1:]
+                else:
+                    negative_sample_buffer_index += 1
+
+                if yt_predict == 1:
+                    # false positive
+                    negative_sample_buffer[negative_sample_buffer_index] = 1
+                else:
+                    # true negative
+                    negative_sample_buffer[negative_sample_buffer_index] = 0
+
+            # estimate the FPR of the current model using the moving buffer
+            if negative_sample_buffer_index == negative_sample_buffer_size-1:
+                estimated_FPR = np.mean(negative_sample_buffer)
+            else:
+                estimated_FPR = np.mean(negative_sample_buffer[:negative_sample_buffer_index+1])
+
+            # y(t), calculate mu(t)
+            if yt == 1:
+                # mu(t) uses gamma(t-1), n_plus(t-1), n_minus(t-1)
+                mu = (transient_number_of_positive_samples + transient_number_of_negative_samples)/transient_number_of_positive_samples
+                # save class costs
+                pos_class_weight_train_array[i] = mu
+                if i>1:
+                    neg_class_weight_train_array[i] = neg_class_weight_train_array[i-1]
+            else:
+                # mu(t) uses gamma(t-1), n_plus(t-1), n_minus(t-1)
+                mu = gamma*(transient_number_of_positive_samples + transient_number_of_negative_samples)/transient_number_of_negative_samples
+                # save class costs
+                neg_class_weight_train_array[i] = mu
+                if i>1:
+                    pos_class_weight_train_array[i] = pos_class_weight_train_array[i-1]
+
+            # SGD
+            # Update perceptron in each node
+            for k in range(len(dark_node_indices)-1,-1,-1):
+                dark_node_index = dark_node_indices[k]
+                # get the node loss
+                z = yt*y_discriminant[dark_node_index]
+                dloss_dz = self.__deriv_sigmoid_loss(z, sigmoid_h)
+                node_loss = self.__sigmoid_loss(z, sigmoid_h)
+                loss = mu*node_loss - gamma*tfpr
+                # update node performance
+                E[dark_node_index] = E[dark_node_index]*np.exp(node_loss_constant*loss)
+                # update node prob
+                if k == len(dark_node_indices)-1:
+                    P[dark_node_index] = E[dark_node_index]
+                else:
+                    P[dark_node_index] = split_prob * P[connectivity[dark_node_index, 2]] * P[connectivity[dark_node_index, 3]] + (1-split_prob)*E[dark_node_index]
+                # SGD on each node
+                dz_dw = yt*xt
+                dz_db = yt
+                dloss_dw = dloss_dz*dz_dw
+                dloss_db = dloss_dz*dz_db
+                # update w and b
+                w[:, dark_node_index] = (1-eta*Lambda)*w[:, dark_node_index]-eta*mu*dloss_dw
+                b[dark_node_index] = b[dark_node_index]-eta*mu*dloss_db
+
+            # y(t)
+            if yt == 1:
+                # calculate n_plus(t)
+                transient_number_of_positive_samples += 1
+            else:
+                # calculate n_minus(t)
+                transient_number_of_negative_samples += 1
+                # calculate gamma(t)
+                gamma = gamma*(1+beta*(estimated_FPR - tfpr))
+
+            # update learning rate of perceptron
+            eta = eta_init/(1+Lambda*(transient_number_of_positive_samples + transient_number_of_negative_samples))
+
+            # update uzawa gain
+            beta = beta_init/(1+Lambda*(transient_number_of_positive_samples + transient_number_of_negative_samples))
+
+        # save updated parameters
+        self.w_ = w
+        self.b_ = b
+        self.P_ = P
+        self.E_ = E
+
+        # save the results
+        self.mu_train_array_ = sample_mu
+        self.tpr_train_array_ = tpr_train_array
+        self.fpr_train_array_ = fpr_train_array
+        self.neg_class_weight_train_array_ = neg_class_weight_train_array
+        self.pos_class_weight_train_array_ = pos_class_weight_train_array
+
+        return None
+
+    def predict(self, X):
+        # note that input should be numpy array, may need to fix this part for other data types
+        n_test = X.shape[0]
+        y_predict = np.empty(n_test)
+        w = self.w_
+        b = self.b_
+        connectivity = self.connectivity_
+        tfpr = self.tfpr
+        P = self.P_
+        E = self.E_
+        projection_type = self.projection_type_
+
+        number_of_nodes = 2**(self.tree_depth_+1)-1
+        sigma_tree = np.zeros((self.tree_depth_+1,))
+        mu_tree = np.zeros((self.tree_depth_+1, 1))
+        y_discriminant = np.zeros((number_of_nodes, 1))
+        C = np.zeros((number_of_nodes, 1))
+        split_prob = self.split_prob_
+
+        for i in range(0, n_test):
+            # take the input data
+            xt = X[i,:]
+
+            # calculate sample propogation path through tree
+            dark_node_indices = self.__find_dark_nodes(xt)
+
+            # if manual projection is used, first 2D are coordinates
+            if projection_type == 'manual':
+                xt = xt[2:]
+
+            # make prediction
+            for k in range(0, len(dark_node_indices)):
+                dark_node_index = dark_node_indices[k]
+                # calculate sigma
+                if k == 0:
+                    sigma_tree[k] = 1-split_prob
+                else:
+                    sigma_tree[k] = (1-split_prob)*P[connectivity[dark_node_index, 1]]*sigma_tree[k-1]
+                    if k == len(dark_node_indices)-1:
+                        sigma_tree[k] = sigma_tree[k]/(1-split_prob)
+                # calculate weights
+                mu_tree[k] = sigma_tree[k]*E[dark_node_index]/P[0]
+                if k == len(dark_node_indices)-1:
+                    mu_tree[k] = 1-np.sum(mu_tree[:k])
+
+                # calculate discriminant in each node
+                y_discriminant_ = np.matmul(xt, w[:, dark_node_index])+b[dark_node_index]
+                y_discriminant[dark_node_index] = y_discriminant_
+                C[dark_node_index] = np.sign(y_discriminant_)
+            
+            # calculate prediction of the ensemble
+            yt_predict = self.__expert_ensemble(C[dark_node_indices], y_discriminant[dark_node_indices], mu_tree)
+            y_predict[i] = yt_predict
+
+        return y_predict
+
     # aux functions
+    def __expert_ensemble(self, expert_predictions, expert_discriminants, expert_weights):
+
+        if self.ensemble_type_ == 'probabilistic':
+            expert_weight_pmf = np.cumsum(expert_weights)
+            uniform_rv = np.random.rand(1)[0]
+            bool_array = (uniform_rv <= expert_weight_pmf)
+            best_expert_index = np.where(bool_array == True)[0][0]
+            ensemble_prediction = expert_predictions[best_expert_index]
+            return ensemble_prediction
+
+        elif self.ensemble_type_ == 'linear':
+            ensemble_discriminant = np.dot(expert_discriminants, expert_weights)
+            ensemble_prediction = np.sign(ensemble_discriminant)
+            return ensemble_prediction
+
     def __find_dark_nodes(self, xt):
         dark_node_indices = np.zeros((self.tree_depth_+1,), dtype=np.int32)
         dark_node_index = 0
@@ -332,7 +603,7 @@ class tree_olnp:
 
         leaf_nodes = []
         for i in range(0, X.shape[0]):
-            xt=X[i,:]
+            xt = X[i,:]
             dark_node_indices = self.__find_dark_nodes(xt)
             leaf_node = dark_node_indices[-1]
             leaf_nodes.append(leaf_node)
